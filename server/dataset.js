93b51a04d3df0cb29076c48c44e1a65b95981067
miguel <miguelj_5@hotmail.com>
2014-11-03 16:40:29 +0000

    index.js improved

--- server/dataset.js
+++ server/dataset.js
@@ -2,40 +2,146 @@ function Dataset() {
 	
 	this.divisions = {
 
-		"bathRoom" : {
+		"livingroom" : {
 
 			"light" : {
 				"status" : "off"
-			}
-		},
+			},
 
-		"livingRoom" : {
-
-			"light" : {
+			"airconditioner" : {
 				"status" : "off"
-			},		
+			},	
 
 			"tv" : {
+				"status" : "off",
+				"volume" : "0"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
 				"status" : "off",
 				"volume" : "0"
 			}
 		},
 
-		"livingRoom" : {
-
-			"airConditioner" : {
-				"status" : "off"
-			},
+		"kitchen" : {
 
 			"light" : {
 				"status" : "off"
-			},		
+			},
+
+			"airconditioner" : {
+				"status" : "off"
+			},	
 
 			"tv" : {
+				"status" : "off",
+				"volume" : "0"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
+				"status" : "off",
+				"volume" : "0"
+			}
+		},
+
+		"room1" : {
+
+			"light" : {
+				"status" : "off"
+			},
+
+			"airconditioner" : {
+				"status" : "off"
+			},	
+
+			"tv" : {
+				"status" : "off",
+				"volume" : "0"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
+				"status" : "off",
+				"volume" : "0"
+			}
+		},
+
+		"room2" : {
+
+			"light" : {
+				"status" : "off"
+			},
+
+			"airconditioner" : {
+				"status" : "off"
+			},	
+
+			"tv" : {
+				"status" : "off",
+				"volume" : "0"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
+				"status" : "off",
+				"volume" : "0"
+			}
+		},
+
+		"bathroom" : {
+
+			"light" : {
+				"status" : "off"
+			},
+
+			"airconditioner" : {
+				"status" : "off"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
+				"status" : "off",
+				"volume" : "0"
+			}
+		},
+
+		"hall" : {
+
+			"light" : {
+				"status" : "off"
+			},
+
+			"airconditioner" : {
+				"status" : "off"
+			},
+
+			"windows" : {
+				"status" : "off"
+			},
+
+			"soundsystem" : {
 				"status" : "off",
 				"volume" : "0"
 			}
 		}
+		
 	};
 }
 
@@ -44,20 +150,80 @@ Dataset.prototype.getDivisionsStatus = function(callback) {
 	return callback( this.divisions );
 }
 
-// Set bathroom status
-Dataset.prototype.setBathRoomStatus = function(device, type, status, callback) {
-	
-	this.divisions['bathRoom'][device][type] = status;
+// Get devices statuses
+/*Dataset.prototype.getDevicesStatus = function(callback) {
+	var devices;
+	var divisions = this.divisions;
 
-	return callback({ success : true });
-}
+	Object.keys(divisions).forEach(function (div) {
+
+		console.log('-> ' + div);
+
+		Object.keys(divisions[div]).forEach(function (device) {
+			console.log('-- ' + device);
+
+			if(! devices[device]) {
+				console.log('wat');
+
+				devices[device] = divisions[div][device];
+			}
+				
+				//devices[device] = divisions[div][device];
+
+		});
+	});
+
+	return callback( this.divisions );
+}*/
 
 // Set livingroom status
 Dataset.prototype.setLivingRoomStatus = function(device, type, status, callback) {
 	
-	this.divisions['livingRoom'][device][type] = status;
+	this.divisions['livingroom'][device][type] = status;
 
 	return callback({ success : true });
 }
+
+// Set kitchen status
+Dataset.prototype.setKitchenStatus = function(device, type, status, callback) {
+	
+	this.divisions['kitchen'][device][type] = status;
+
+	return callback({ success : true });
+}
+
+// Set room1 status
+Dataset.prototype.setRoom1Status = function(device, type, status, callback) {
+	
+	this.divisions['room1'][device][type] = status;
+
+	return callback({ success : true });
+}
+
+// Set room2 status
+Dataset.prototype.setRoom2Status = function(device, type, status, callback) {
+	
+	this.divisions['room2'][device][type] = status;
+
+	return callback({ success : true });
+}
+
+// Set bathroom status
+Dataset.prototype.setBathRoomStatus = function(device, type, status, callback) {
+	
+	this.divisions['bathroom'][device][type] = status;
+
+	return callback({ success : true });
+}
+
+// Set hall status
+Dataset.prototype.setHallStatus = function(device, type, status, callback) {
+	
+	this.divisions['hall'][device][type] = status;
+
+	return callback({ success : true });
+}
+
+
 
 module.exports = Dataset;
--- server/index.js
+++ server/index.js
@@ -2,33 +2,64 @@ var dataset = require('./dataset.js');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/divisions', function (req, res) {
 
 	dataset.getDivisionsStatus(function(callback) {
 		res.json(callback);
 	});
 });
 
-// exemplo : /bathroom/light/status/on
-app.get('/bathroom/:device/:type/:status', function (req, res) {
+/*app.get('/devices', function (req, res) {
+
+	dataset.getDevicesStatus(function(callback) {
+		res.json(callback);
+	});
+});*/
+
+// eg. : /livingroom/tv/volume/4
+app.get('/:room/:device/:type/:status', function (req, res) {
+	var room = req.params.room;
 	var device = req.params.device;
 	var type = req.params.type;
 	var status = req.params.status;
 
-	dataset.setBathRoomStatus(device, type, status, function(callback) {
-		res.json(callback);
-	});
-});
+	switch(room) {
+		case 'livingroom':
+			dataset.setLivingRoomStatus(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
 
-// exemplo : /livingroom/tv/volume/4
-app.get('/livingroom/:device/:type/:status', function (req, res) {
-	var device = req.params.device;
-	var type = req.params.type;
-	var status = req.params.status;
+		case 'kitchen':
+			dataset.setKitchenStatus(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
 
-	dataset.setLivingRoomStatus(device, type, status, function(callback) {
-		res.json(callback);
-	});
+		case 'room1':
+			dataset.setRoom1Status(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
+
+		case 'room2':
+			dataset.setRoom2Status(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
+
+		case 'bathroom':
+			dataset.setBathRoomStatus(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
+
+		case 'hall':
+			dataset.setHallStatus(device, type, status, function(callback) {
+				res.json(callback);
+			});
+			break;
+	}
 });
 
 var server = app.listen(3000, function () {
